32dab1b08edaf228d71208da1ae54ad46ecd30e6
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-23 21:29:21 +0300

    Update StatsBomb.py

--- StatsBomb.py
+++ StatsBomb.py
@@ -1145,7 +1145,7 @@ with tab1:
             st.dataframe(pass_btn, hide_index=True)
         else:
             st.warning("لا توجد بيانات تمريرات للعرض.")
-    except Exception as e:
+        except Exception as e:
             st.error(f"خطأ في إنشاء شبكة التمريرات: {str(e)}")
     elif an_tp == 'مناطق الهجوم':
         st.subheader('تحليل مناطق الهجوم')
