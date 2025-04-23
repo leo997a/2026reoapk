b51a83e23bd7096836b390ea7d495dd86c8bce7f
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-23 20:10:14 +0300

    Update StatsBomb.py

--- StatsBomb.py
+++ StatsBomb.py
@@ -1179,8 +1179,8 @@ with tab1:
         
         # إنشاء الرسم
         fig, ax = plt.subplots(figsize=(10, 10), facecolor=bg_color)
-        fig.tight_layout(pad=0.2)  # تقليل الهوامش
-        fig.subplots_adjust(left=0.03, right=0.97, top=0.95, bottom=0.05)  # ضبط حدود الشكل
+        fig.tight_layout(pad=0.1)  # تقليل الهوامش
+        fig.subplots_adjust(left=0.02, right=0.88, top=0.88, bottom=0.03)  # ضبط حدود الشكل
         # استدعاء pass_network مع جميع الوسائط المطلوبة
         pass_btn = pass_network(
             ax,
