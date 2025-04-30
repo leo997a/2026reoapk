0c00b736b41cbc66a372c8ab67a199a3f4fd4912
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-30 03:59:02 +0300

    Update StatsBomb.py

--- StatsBomb.py
+++ StatsBomb.py
@@ -1502,8 +1502,8 @@ with tab1:
         st.pyplot(fig)
 
     elif an_tp == reshape_arabic_text('PPDA'):
-    st.subheader(reshape_arabic_text('معدل الضغط (PPDA)'))
-    st.write(reshape_arabic_text("PPDA: عدد التمريرات الناجحة التي يسمح بها الفريق مقابل كل فعل دفاعي في الثلث الدفاعي للخصم. القيمة الأقل تشير إلى ضغط دفاعي أقوى (عادة 5-15)."))
+        st.subheader(reshape_arabic_text('معدل الضغط (PPDA)'))
+        st.write(reshape_arabic_text("PPDA: عدد التمريرات الناجحة التي يسمح بها الفريق مقابل كل فعل دفاعي في الثلث الدفاعي للخصم. القيمة الأقل تشير إلى ضغط دفاعي أقوى (عادة 5-15)."))
 
     # اختيار الفترة
     period_choice = st.selectbox(
