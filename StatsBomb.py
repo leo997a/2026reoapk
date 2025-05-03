130fa32352396ae56eaa7d8cd511c68af6912a6d
leo997a <166352340+leo997a@users.noreply.github.com>
2025-05-03 20:07:09 +0300

    Update StatsBomb.py

--- StatsBomb.py
+++ StatsBomb.py
@@ -1613,9 +1613,8 @@ def plot_match_stats(ax, df, hteamName, ateamName, hcol, acol, bg_color, line_co
         st.write(f"فرق موجودة في البيانات: {df['teamName'].unique()}")
     
     
-    Returns:
-    pd.DataFrame
-        إطار بيانات يحتوي على إحصائيات الفريقين
+    return pd.DataFrame([home_data, away_data])
+
     
     # إعداد الملعب
     pitch = Pitch(pitch_type='uefa', corner_arcs=True, pitch_color=bg_color, line_color=bg_color, linewidth=2)
